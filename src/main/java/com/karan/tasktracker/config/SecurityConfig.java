427a69bfb84cf8dec42a7cc5ce1403bf9877edbb
aman3380 <149221470+aman3380@users.noreply.github.com>
2026-07-14 11:47:30 +0530

    Update SecurityConfig.java

--- src/main/java/com/karan/tasktracker/config/SecurityConfig.java
+++ src/main/java/com/karan/tasktracker/config/SecurityConfig.java
@@ -65,6 +65,35 @@ public class SecurityConfig {
         return source;
     }
     @Bean
+public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowedOrigins(List.of(
+            "http://127.0.0.1:5500",
+            "http://localhost:5500"
+    ));
+
+    config.setAllowedMethods(List.of(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "PATCH",
+            "OPTIONS"
+    ));
+
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", config);
+
+    return source;
+}
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
         return configuration.getAuthenticationManager();
     }
